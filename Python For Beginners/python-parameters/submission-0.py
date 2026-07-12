c4d4e884ee931d2b9ea8d45655040a8051895c4b
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-07-12 13:17:05 -0700

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,8 @@
+def farwell(name):
+    message = "Goodbye, " + name
+    return message
+
+# don't modify below this line
+print(farwell("Bob"))
+print(farwell("Charlie"))
+print(farwell("NeetCode"))
