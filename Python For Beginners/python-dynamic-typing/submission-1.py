c3022bafef0ad5e2057df3a4d00f39219c2b1379
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-07-12 00:03:51 -0700

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable)) #prints a int 
+variable = 3.14
+print(type(variable))#prints float 
+variable = True
+print(type(variable))#prints a boolean 
+variable = "This is a String" 
+print(type(variable))#prints a string 
+variable = [] 
+print(type(variable)) #prints a list type 
