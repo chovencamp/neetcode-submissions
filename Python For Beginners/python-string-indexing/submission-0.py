c1906aa8e2721ac112106e1ee0539571945b33b6
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-07-13 23:52:28 -0700

    Add: python-string-indexing - submission-0

---
+++ Python For Beginners/python-string-indexing/submission-0.py
@@ -0,0 +1,18 @@
+def print_first_char(word: str) -> None:
+    print(word[0]) #prints the first char in the string 
+
+def print_second_char(word: str) -> None:
+    print(word[1]) #prints the second char in the string 
+
+def print_last_char(word: str) -> None:
+    print(word[len(word) - 1]) #prints the length - 1 which gives the last number
+
+
+# do not modify below this line
+print_first_char("hello")
+print_second_char("hello")
+print_last_char("hello")
+
+print_first_char("yay")
+print_second_char("yay")
+print_last_char("yay")
