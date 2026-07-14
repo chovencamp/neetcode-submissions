b91eebcd75a63ca3451cbe25139fcea008ae0730
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-07-13 22:27:57 -0700

    Add: python-control-flow - submission-1

---
+++ Python For Beginners/python-control-flow/submission-1.py
@@ -0,0 +1,23 @@
+'''This is mean to show 
+that nothing else happened. The only thing that happens 
+here is that the code prints at the end the control statement 
+skip over ever doing anything
+'''
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    break
+    print(i)
+
+for i in range(1, 8):
+    continue
+    print(i)
+
+print("nothing else happened")
