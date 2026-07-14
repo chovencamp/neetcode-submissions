b0bcdfd39db0114e96c3feb6ab24c73178298dd5
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-07-13 22:37:51 -0700

    Add: python-length-function - submission-2

---
+++ Python For Beginners/python-length-function/submission-2.py
@@ -0,0 +1,21 @@
+''' This program takes the two strings and finds which one is longer and 
+returns that. If they are equal in length it will return the first word 
+'''
+def get_longer_word(word1: str, word2: str) -> str:
+    if len(word1) > len(word2):
+        return word1
+    elif len(word2) > len(word1):
+        return word2
+    elif len(word2) == len(word1):
+        return word1
+
+''' It can also be done like this as well'''
+def get_longer_word(word1: str, word2: str) -> str:
+    if len(word1) >= len(word2):
+        return word1 
+    return word2
+
+# do not modify below this line
+print(get_longer_word("yellow", "orange"))
+print(get_longer_word("red", "blue"))
+print(get_longer_word("green", "blue"))
