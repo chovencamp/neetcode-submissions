e10c615c13863ba948ba2f069dc54b4268fb651d
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-08-31 13:04:14 -0700

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    user_integer = int(input())
+    return user_integer
+
+
+def read_float() -> float:
+    user_float = float(input())
+    return user_float
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
