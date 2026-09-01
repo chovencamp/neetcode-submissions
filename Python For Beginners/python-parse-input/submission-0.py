b720beb0521c1a13f836a2b8d9ee3650a973957d
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-08-31 17:32:09 -0700

    Add: python-parse-input - submission-0

---
+++ Python For Beginners/python-parse-input/submission-0.py
@@ -0,0 +1,22 @@
+from typing import List
+
+def read_integers() -> List[int]:
+    user_input = input()
+    #removes the commas and from what the input was stored 
+    strings = user_input.split(",")
+    #create the list that we will use 
+    int_list = []
+
+    #loop on the strings 
+    for string in strings: 
+        #store each char in the string to the list but type cast it as its currently a string when we need to return a list of ints 
+        int_list.append(int(string))
+
+    return int_list
+
+    
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
