e77296b4a673dad0088ef6df152d2404f06d3f1a
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-08-31 17:37:56 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,19 @@
+def add_two_numbers() -> int:
+    #convert to a list with the split() funciton 
+    user_input = input().split(",")
+
+    #since it was converted to a list find the first number stored via its index 
+    numberOne = user_input[0]
+    #find the second number via its index 
+    numberTwo = user_input[1]
+
+    return int(numberOne) + int(numberTwo)
+
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
