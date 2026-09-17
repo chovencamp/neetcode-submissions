78e31fd0ed265001b1baf147835db742eb168e0b
Chris Hovencamp <42745787+chovencamp@users.noreply.github.com>
2026-09-16 18:24:55 -0700

    Add: python-queue-enqueue-dequeue - submission-1

---
+++ Python Coding Interviews/python-queue-enqueue-dequeue/submission-1.py
@@ -0,0 +1,23 @@
+from typing import List, Deque
+from collections import deque
+
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    #convert the list to a deque 
+    queue = deque(arr)
+    #loop k time and store it in i 
+    for i in range(k):
+        #pop left from the queue for k times 
+        #this will pop left and the append to the right rotating the list 
+        queue.append(queue.popleft())
+    return queue 
+
+
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
